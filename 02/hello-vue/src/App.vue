58349b8cc779a67ee0260bce388a8797899a3a33
hiroto-hara <xre0605@gmail.com>
2023-09-03 23:02:09 +0900

    start vue project

---
+++ 02/hello-vue/src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref("田中太郎")
+</script>
+
+<template>
+  <h1>こんにちは!{{name}}さん!</h1>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>

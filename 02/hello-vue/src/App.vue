1d055f19a4afd36767f88a5bbaf0fb3eebfc4901
hiroto-hara <xre0605@gmail.com>
2023-09-14 21:08:28 +0900

    双方向データバインディング

--- 02/hello-vue/src/App.vue
+++ 02/hello-vue/src/App.vue
@@ -1,39 +1,12 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const name = ref("田中太郎")
-const greeting = ref("こんにちは")
+const inputNameModel = ref("双方向")
 </script>
 
 <template>
-  <h1>{{ greeting }}!{{name}}さん!</h1>
+  <section>
+    <input type="text" v-model="inputNameModel">
+    <p>{{ inputNameModel }}</p>
+  </section>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>

f7865354e2eb1a86f69fe65f401962173e0d3438
hiroto-hara <xre0605@gmail.com>
2023-09-28 21:37:42 +0900

    v-if

--- 02/hello-vue/src/App.vue
+++ 02/hello-vue/src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref, shallowReactive } from "vue";
 
-const inputNameModel = ref("双方向")
+const number = ref(80)
+const showOrNot = computed(
+  (): boolean => {
+    let showOrNot = false
+    const rand = Math.round(Math.random() * 100)
+    if(rand >= 50) {
+      showOrNot = true
+    }
+    return showOrNot
+  }
+)
 </script>
 
 <template>
-  <section>
-    <input type="text" v-model="inputNameModel">
-    <p>{{ inputNameModel }}</p>
-  </section>
+  <p v-if="number >= 50">あ</p>
+  <p v-if="Math.round(Math.random() * 100) >= 50">い</p>
+  <p v-if="showOrNot">う</p>
 </template>
